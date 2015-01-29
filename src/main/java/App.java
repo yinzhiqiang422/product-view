1a981c4a8e510ca8aed580d7ab95e2c8d2c0a8d0
Albert Cardona <acardonao@gmail.com>
2015-01-29 08:07:57 -0500

    clase iniciadora spring boot

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
